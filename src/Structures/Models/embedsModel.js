db1513911cdda383276f701333077fa7b6f61be8
hoki <Hoki010010@gmail.com>
2023-12-06 10:16:51 +0300

    add/modelbd

---
+++ src/Structures/Models/embedsModel.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../Utility/database.js')
+
+const embedsModel = sequelize.define(
+    'embedsModel',
+    {
+        executor: {
+            type: DataTypes.STRING, // кто сменил эмбед
+            allowNull: false
+        },
+        embed: {
+            type: DataTypes.STRING, // сам эмбед
+            allowNull: true
+        },
+        type: {
+            type: DataTypes.STRING, // откуда этот эмбед
+            allowNull: false
+        },
+        haveEmbeds: {
+            type: DataTypes.BOOLEAN, // есть ли в эмбеде "embeds"
+            allowNull: false
+        },
+    },
+    {
+        tableName: 'embedsModel',
+        // freezeTableName: true, выключает автоматическую плюрализацию
+        updatedAt: true,
+
+    }
+)
+
+module.exports = embedsModel;
